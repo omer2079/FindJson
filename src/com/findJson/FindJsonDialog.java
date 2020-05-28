ba78113e76a25f8db856ce7b7bdecf86e80892ee
Omer Priel <omer2079@gmail.com>
2020-05-28 15:52:02 +0300

    add title items

--- src/com/findJson/FindJsonDialog.java
+++ src/com/findJson/FindJsonDialog.java
@@ -1,8 +1,8 @@
 package com.findJson;
 
-import com.intellij.openapi.ui.ComboBox;
 import com.intellij.openapi.ui.DialogWrapper;
 import com.intellij.ui.SearchTextField;
+import com.intellij.ui.components.JBCheckBox;
 import com.intellij.ui.table.JBTable;
 import org.jetbrains.annotations.Nullable;
 
@@ -24,6 +24,9 @@ class FindJsonDialog extends DialogWrapper {
     private JPanel mainPanel;
 
     private JPanel titlePanel;
+    private JLabel titleLabel;
+    private JCheckBox matchCaseBox;
+    private JCheckBox fromHeadBox;
     private SearchTextField findBox;
     private JPanel findInFolderPanel;
     private JBTable foundFilesList;
@@ -36,17 +39,18 @@ class FindJsonDialog extends DialogWrapper {
         // Window Config
         this.setTitle("Find In Json");
         this.setModal(false);
+        this.setUndecorated(true);
 
         window = this.getWindow();
         window.addWindowListener(new WindowAdapter() {
             @Override
             public void windowClosing(WindowEvent e) {
-                //window.setVisible(false);
+                window.setVisible(false);
             }
 
             @Override
             public void windowDeactivated(WindowEvent e) {
-                //window.setVisible(false);
+                window.setVisible(false);
             }
         });
 
@@ -55,10 +59,31 @@ class FindJsonDialog extends DialogWrapper {
         mainPanel.setPreferredSize(new Dimension(610, 660));
         mainPanel.setMinimumSize(new Dimension(425, 325));
         mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.Y_AXIS));
+        mainPanel.setBorder(BorderFactory.createEmptyBorder(5,5,5,5));
 
         // titlePanel
         titlePanel = new JPanel();
         setComponentSize(titlePanel, 40, 40, 40);
+        titlePanel.setLayout(new BoxLayout(titlePanel, BoxLayout.X_AXIS));
+
+        // titleLabel
+        titleLabel = new JLabel();
+        titleLabel.setBorder(BorderFactory.createEmptyBorder(0, 10, 0,0));
+        titleLabel.setText(this.getTitle());
+
+        // matchCaseBox
+        matchCaseBox = new JBCheckBox("Match Case", true);
+        matchCaseBox.setBorder(BorderFactory.createEmptyBorder(0, 0, 0,20));
+
+        // fromHeadBox
+        fromHeadBox = new JBCheckBox("From Head", true);
+        fromHeadBox.setBorder(BorderFactory.createEmptyBorder(0, 0, 0,10));
+
+        titlePanel.add(titleLabel);
+        titlePanel.add(Box.createHorizontalGlue());
+        titlePanel.add(new JTextField());
+        titlePanel.add(matchCaseBox);
+        titlePanel.add(fromHeadBox);
 
         // TODO findBox inner panel
         // findBox
@@ -81,7 +106,6 @@ class FindJsonDialog extends DialogWrapper {
         // foundFileBody
         foundFileBody = new JTextArea();
 
-        // Load Panel
         mainPanel.add(titlePanel);
         mainPanel.add(findBox);
         mainPanel.add(findInFolderPanel);
@@ -112,6 +136,6 @@ class FindJsonDialog extends DialogWrapper {
 
     @Override
     public void show() {
-        window.setVisible(true);
+        super.show();
     }
 }
